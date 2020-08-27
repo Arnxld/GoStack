4652deda0ca8696bbffe17bb94998afba89d7236
Arnxld <pedro.arnold13@gmail.com>
2020-08-27 14:13:24 -0300

    stopped at 'Aplicaçao Funcional'

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -2,10 +2,44 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/projects', (request, response) => {
+    const query = request.query
+    console.log(query)
+
     return response.json([
         'Projeto 1',
         'Projeto 2'
+    ])
+})
+
+app.post('/projects', (request, response) => {
+    body = request.body
+
+    console.log(body)
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.put('/projects/:id', (request, response) => {
+    const params = request.params
+
+    console.log(params)
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ])
+})
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3'
     ])
 })
 
